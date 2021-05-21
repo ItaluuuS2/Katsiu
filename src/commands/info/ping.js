ce04d6eb6cf74e60252db02d485800ecfdaa885e
ItaluuuS2 <fitalo238@gmail.com>
2021-05-21 08:23:41 -0300

    Erros

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -1,8 +1,10 @@
+const Discord = require("discord.js");
+0
 module.exports = {
   name: 'ping',
   description: 'Veja meu ping',
   aliases: [''],
   run: async(client, message, args) => {
-    message.channel.send(`**🏓 | Meu ping é de ``${client.ws.ping}``.**`)
+    message.channel.send(`🏓 | Meu ping é de **${client.ws.ping}**`)
   }
 }
