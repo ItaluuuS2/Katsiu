fdc52d8fd55373a8c35d8a97775b1569df7eb4fe
ItaluuuS2 <fitalo238@gmail.com>
2021-05-20 22:10:33 -0300

    Erros

--- src/commands/utils/avatar.js
+++ src/commands/utils/avatar.js
@@ -5,7 +5,6 @@ module.exports = {
   description: 'Veja seu avatar, ou de um usuário.',
   aliases: ['av'],
   run: async(client, message, args) => {
-  try {
  const user = message.mentions.users.first() || (!isNaN(args[0])?await client.users.fetch(String(args[0])):message.author)
  
  const embed = new Discord.MessageEmbed()
@@ -14,8 +13,6 @@ module.exports = {
  .setColor(RANDOM)
  .setFooter(`${message.author.tag}`, message.author.displayAvatarURL())
  message.channel.send(embed)
-  } catch {
-    message.channel.send(`**❌ │Não encontrei esse usuário.**`)
+  
   }
- }
 }
